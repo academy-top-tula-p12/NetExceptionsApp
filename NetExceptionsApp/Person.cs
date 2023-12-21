7815f9c977b86e904b5763a19fbc33f896fb953e
maxefimov <efimov.top.academy@gmail.com>
2023-12-21 21:04:55 +0300

    Добавьте файлы проекта.

---
+++ NetExceptionsApp/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExceptionsApp
+{
+    internal class Person
+    {
+        public string? Name { get; set; }
+        int age;
+        public int Age {
+            get => age;
+            set
+            {
+                if (age > 0)
+                    age = value;
+                else
+                    throw new PersonAgeException("Age must be more zero!");
+            }
+        }
+    }
+
+    class PersonAgeException : Exception
+    {
+        public PersonAgeException(string message) : base(message) { }
+    }
+}
---
+++ NetExceptionsApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using NetExceptionsApp;
+
+Person person = new Person();
+
+try
+{
+    person.Name = "Bobby";
+    person.Age = -1;
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.InnerException);
+    Console.WriteLine(e.Source);
+    Console.WriteLine(e.TargetSite);
+    Console.WriteLine(e.StackTrace);
+}
+
+
+
+/*
+if(Int32.TryParse(Console.ReadLine(), out int number))
+{
+    try
+    {
+
+        Console.WriteLine($"{100 / number}");
+    }
+    catch (DivideByZeroException e)
+    {
+        Console.WriteLine($"{e.Message}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error! {e.Message}");
+    }
+    finally
+    {
+        Console.WriteLine("Block finally");
+    }
+}
+else
+    Console.WriteLine("Input integer number!");
+*/
+
+Console.WriteLine("Other code");
